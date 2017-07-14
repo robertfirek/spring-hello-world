b4377e918649790af20d3c289b3fb7dcf23aa16b
Robert Firek <robert.firek@gmail.com>
2017-07-14 16:19:59 +0100

    Added Hello World application

---
+++ src/main/java/org/firek/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.firek;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
